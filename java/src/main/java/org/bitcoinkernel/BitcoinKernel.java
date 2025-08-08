1beec963e9355fcc2f483d03721d0b8c1082358a
Yuvraj <yuvichh01@gmail.com>
2025-08-08 14:45:51 +0530

    java: add class in a single file for simplicity

--- java/src/main/java/org/bitcoinkernel/BitcoinKernel.java
+++ java/src/main/java/org/bitcoinkernel/BitcoinKernel.java
@@ -1,1 +1,27 @@
 package org.bitcoinkernel;
+
+import static org.bitcoinkernel.BitcoinKernelBindings.*
+
+public class BitcoinKernel {
+    
+}
+
+public class ChainstateManager {
+    
+}
+
+public class ContextManager {
+    
+}
+
+public class Logger {
+    
+}
+
+public class NotificationManager {
+    
+}
+
+public class KernelUtil {
+    
+}
