1a5a0f93fa799f341c19485e55c4e6e3c268d67a
yuvicc <yuvichh01@gmail.com>
2025-10-08 20:58:06 +0530

    cpp: complete block classes

--- cpp/include/block.hpp
+++ cpp/include/block.hpp
@@ -1,5 +1,17 @@
 #pragma once
 
+#include <array>
+#include <functional>
+#include <memory>
+#include <optional>
+#include <span>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
 #include "types.hpp"
 
 namespace btck {
@@ -67,15 +79,81 @@ public:
 class BlockTreeEntry : public View<btck_BlockTreeEntry>
 {
 public:
+    BlockTreeEntry(const btck_BlockTreeEntry* entry)
+        : View{entry}
+    {
+    }
+
+    std::optional<BlockTreeEntry> GetPrevious() const
+    {
+        auto entry{btck_block_tree_entry_get_previous(get())};
+        if (!entry) return std::nullopt;
+        return entry;
+    }
+
+    int32_t GetHeight() const
+    {
+        return btck_block_tree_entry_get_height(get());
+    }
+
+    BlockHash GetHash() const
+    {
+        return BlockHash{btck_block_tree_entry_get_block_hash(get())};
+    }
+
+    friend class ChainMan;
+    friend class Chain;
+}
+
+class BlockSpentOutputs : Handle<btck_BlockSpentOutputs, btck_block_spent_outputs_copy, btck_block_spent_outputs_destroy>
+{
+public:
+    BlockSpentOutputs(btck_BlockSpentOutputs* block_spent_outputs)
+        : Handle{block_spent_outputs}
+    {
+    }
+
+    size_t Count() const
+    {
+        return btck_block_spent_outputs_count(get());
+    }
+
+    TransactionSpentOutputsView GetTxSpentOutputs(size_t tx_undo_index) const
+    {
+        return TransactionSpentOutputsView{btck_block_spent_outputs_get_transaction_spent_outputs_at(get(), tx_undo_index)};
+    }
+
+    auto TxsSpentOutputs() const
+    {
+        return Range<BlockSpentOutputs, &BlockSpentOutputs::Count, &BlockSpentOutputs::GetTxSpentOutputs>{*this};
+    }
 
 
 }
 
 
 
-class BlockSpentOutputs;
-class BlockValidationState;
+class BlockValidationState
+{
+private:
+    const btck_BlockValidationState* m_state;
 
+public:
+    BlockValidationState(const btck_BlockValidationState* state) : m_state{state} {}
 
+    BlockValidationState(const BlockValidationState&) = delete;
+    BlockValidationState& operator=(const BlockValidationState&) = delete;
+    BlockValidationState(const BlockValidationState&&) = delete;
+    BlockValidationState& operator=(const BlockValidationState&&) = delete;
 
+    ValidationMode GetValidationMode() const
+    {
+        return static_cast<ValidationMode>(btck_block_validation_state_get_validation_mode(m_state));
+    }
+
+    BlockValidationResult GetBlockValidationResult() const
+    {
+        return static_cast<BlockValidationResult>(btck_block_validation_state_get_block_validation_result(m_state));
+    }
+};
 }
